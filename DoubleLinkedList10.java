9844b98d1d6ee36271f3a54b01384d674874be69
meylaniic <meylaniic@gmail.com>
2025-05-28 10:04:10 +0700

    first commit

---
+++ DoubleLinkedList10.java
@@ -0,0 +1,89 @@
+public class DoubleLinkedList10 {
+    Node10 head;
+    Node10 tail;
+
+    public DoubleLinkedList10() {
+        head = null;
+        tail = null;
+    }
+    
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa10 data) {
+        Node10 newNode = new Node10(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa10 data) {
+        Node10 newNode = new Node10(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa10 data) {
+        Node10 current = head;
+
+        while (current != null && !current.data.nama.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Node dengan NIM " +keyNim + " tidak ditemukan.");
+            return;
+        }
+
+        Node10 newNode = new Node10(data);
+
+        if (current==tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        System.out.println("Node Berhasil Disisipkan Setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        Node10 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+    public void removeFirst() {
+
+    }
+
+    public void removeLast() {
+
+    }
+
+    public Node10 search(String nim) {
+        Node10 current = head;
+        while (current != null) {
+            if (current.data.nim.equals(nim)) {
+                return current;
+            }
+        }
+        return null;
+    }
+}
+
